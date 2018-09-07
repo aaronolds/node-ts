99f58d2e95a42c8b50d90e6da3a38251c775ed12
Aaron P. Olds <aaron.olds@gmail.com>
2018-09-07 16:11:52 -0400

    tests are working

--- tests/connection.test.ts
+++ tests/connection.test.ts
@@ -1,18 +1,16 @@
+import request from 'supertest';
 import app from '../src/index'; // If I don't include the app it will fail
-import http from 'http'; // why does this have to be require???  If I try to import it fails
-import { expect, use, request } from 'chai';
-import chaiHttp from 'chai-http';
-
-use(chaiHttp);
+import chai from 'chai';
+const expect = chai.expect;
 
 describe('test', function() {
   it('should return 200', function(done) {
     // <= Pass in done callback
-    request('http://localhost:3000')
+    request(app)
       .get('/')
       .end(function(err, res) {
         expect(err).to.be.null;
-        expect(res).to.have.status(200);
+        expect(res.status).to.eq(200);
         done(); // <= Call done to signal callback end
       });
   });
